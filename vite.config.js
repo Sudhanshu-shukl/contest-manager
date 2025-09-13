467e875583601f0fe6071d4a36737f9868d05fe4
Sudhanshu-shukl <shuklasudhanshu230405@gmail.com>
2025-09-14 02:32:24 +0530

    Deployment 102

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/contest-manager/',
+  base: process.env.NODE_ENV === 'production' ? '/contest-manager/' : '/',
   server: {
     host: '0.0.0.0', // Allow external connections
     port: 5173,
